864200d1f04b2efce8ff0edfd0817de0210a8a1f
Michael Davidsaver <mdavidsaver@bnl.gov>
2009-07-22 13:26:28 -0400

    Imported Upstream version 1.3

---
+++ telnetd.mod.c
@@ -0,0 +1,23 @@
+/* $Id: telnetd.mod.c,v 1.3 2007-05-09 22:41:03 till Exp $ */
+
+/* cexpsh loadable module support */
+
+/* T. Straumann <strauman@slac.stanford.edu>, 2003-2007 */
+#include <cexp.h>
+
+int startTelnetd(void (*cmd)(char *, void *), void *arg, int dontSpawn, int stack, int priority);
+
+static void
+cexpWrap(char *dev, void *arg)
+{
+char	*args[]={"Cexp-telnet",0};
+	fprintf(stderr,"[Telnet:] starting cexp on %s\n",dev);
+	cexp_main(1,args);
+}
+
+void _cexpModuleInitialize(void *m)
+{
+	startTelnetd(cexpWrap,0,0,32000,0);
+}
+
+/* telnetd can't be stopped */
